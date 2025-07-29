dcea98c12ba426e28e21bc47b74f83f6ecb83f06
Pawan <pawan@MacBookAir.lan>
2025-07-29 13:04:00 +0530

    added login page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,6 +3,7 @@ import Dashboard from "./page/admin/dashboard";
 import Order from "./page/admin/order";
 import Product from "./page/admin/product";
 import AdminLayout from "./page/admin/adminLayout";
+import LoginPage from "./page/login";
 
 export default function App() {
   return (
@@ -12,6 +13,8 @@ export default function App() {
         <Route path="product" element={<Product />} />
         <Route path="order" element={<Order />} />
       </Route>
+
+      <Route path="/login" element={<LoginPage />} />
     </Routes>
   );
 }
