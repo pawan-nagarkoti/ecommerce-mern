10ed6bce500dec1c56b56164bdb27d941af83724
Pawan <pawan@MacBookAir.lan>
2025-07-29 13:31:19 +0530

    added ui for admin dashboard

--- client/src/page/admin/adminLayout.jsx
+++ client/src/page/admin/adminLayout.jsx
@@ -14,32 +14,41 @@ import {
   SidebarTrigger,
 } from "@/components/ui/sidebar";
 import { Outlet } from "react-router-dom";
+import { Button } from "@/components/ui/button";
 
 export default function AdminLayout() {
   return (
     <SidebarProvider>
       <AppSidebar />
       <SidebarInset>
-        <header className="flex h-16 shrink-0 items-center gap-2 border-b">
+        <header className="flex h-16 shrink-0 items-center gap-2 border-b justify-between p-4">
           <div className="flex items-center gap-2 px-3">
             <SidebarTrigger />
             <Separator orientation="vertical" className="mr-2 h-4" />
-            <Breadcrumb>
-              <BreadcrumbList>
-                <BreadcrumbItem className="hidden md:block">
-                  <BreadcrumbLink href="#">
-                    Building Your Application
-                  </BreadcrumbLink>
-                </BreadcrumbItem>
-                <BreadcrumbSeparator className="hidden md:block" />
-                <BreadcrumbItem>
-                  <BreadcrumbPage>Data Fetching</BreadcrumbPage>
-                </BreadcrumbItem>
-              </BreadcrumbList>
-            </Breadcrumb>
           </div>
+
+          <Button type="button">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              strokeWidth="2"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+            >
+              <path d="M9 21H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h4" />
+              <polyline points="16 17 21 12 16 7" />
+              <line x1="21" y1="12" x2="9" y2="12" />
+            </svg>
+            Logout
+          </Button>
         </header>
-        <Outlet />
+        <div className="p-5">
+          <Outlet />
+        </div>
       </SidebarInset>
     </SidebarProvider>
   );
--- client/src/page/admin/dashboard.jsx
+++ client/src/page/admin/dashboard.jsx
@@ -1,5 +1,27 @@
-import React from "react";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { Button } from "@/components/ui/button";
 
 export default function dashboard() {
-  return <div>dashboard</div>;
+  return (
+    <>
+      <div className="grid w-full max-w-sm items-center gap-3">
+        <Label htmlFor="picture">Upload Image</Label>
+        <Input id="picture" type="file" />
+        <Button>Upload</Button>
+      </div>
+
+      <div className="mt-10">
+        <img
+          src="https://picsum.photos/200"
+          alt=""
+          className="w-full h-[300px]"
+        />
+        <div className="flex gap-2 mt-2">
+          <Button>Edit</Button>
+          <Button className="bg-red-500 hover:bg-red-400">Delete</Button>
+        </div>
+      </div>
+    </>
+  );
 }
