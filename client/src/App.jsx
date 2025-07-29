d6464a4d370de4b50522ed12bc870be60aba091d
Pawan <pawan@MacBookAir.lan>
2025-07-29 13:00:00 +0530

    fixed sidebar opening pages issue

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,11 +2,13 @@ import { Route, Routes } from "react-router-dom";
 import Dashboard from "./page/admin/dashboard";
 import Order from "./page/admin/order";
 import Product from "./page/admin/product";
+import AdminLayout from "./page/admin/adminLayout";
 
 export default function App() {
   return (
     <Routes>
-      <Route path="/admin" element={<Dashboard />}>
+      <Route path="/admin" element={<AdminLayout />}>
+        <Route index element={<Dashboard />} />
         <Route path="product" element={<Product />} />
         <Route path="order" element={<Order />} />
       </Route>
