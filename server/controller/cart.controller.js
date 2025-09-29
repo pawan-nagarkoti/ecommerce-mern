79d37308d140f683cf944fa3a3173921ff271548
Pawan <pawan@Pawans-MacBook-Air.local>
2025-09-29 08:29:38 +0530

    added api for quntity update in cart

--- server/controller/cart.controller.js
+++ server/controller/cart.controller.js
@@ -96,19 +96,68 @@ const fetchCartData = async (req, res) => {
 };
 
 const deleteCartItem = async (req, res) => {
-  const deletedID = req.params.id;
+  try {
+    const deletedID = req.params.id;
 
-  const itemDelete = await Cart.findByIdAndDelete(deletedID);
+    const itemDelete = await Cart.findByIdAndDelete(deletedID);
 
-  if (itemDelete) {
-    return res.status(200).json({
-      data: itemDelete,
-      message: "Item deleted",
-    });
-  } else {
-    return res.status(400).json({
-      message: "Not found",
-    });
+    if (itemDelete) {
+      return res.status(200).json({
+        data: itemDelete,
+        message: "Item deleted",
+      });
+    } else {
+      return res.status(400).json({
+        message: "Not found",
+      });
+    }
+  } catch (e) {
+    console.log(e.message);
+  }
+};
+
+const updateCartItem = async (req, res) => {
+  try {
+    const { id, sign, price } = req.body;
+    const data = await Cart.find({ _id: id });
+
+    switch (sign) {
+      case "increment":
+        const quantityIncrease = data.map((v) => v.quantity + 1);
+        const cartUpdateAfterIncrement = await Cart.findByIdAndUpdate(
+          id,
+          {
+            quantity: quantityIncrease[0],
+            totalPrice: quantityIncrease * price,
+          },
+          { new: true }
+        );
+        res.status(200).json({
+          data: cartUpdateAfterIncrement,
+          message: "quntity increase",
+        });
+        break;
+      case "decrement":
+        const quntityDecrease = data.map((v) => v.quantity - 1);
+        const cartUpdateAfterDecrement = await Cart.findByIdAndUpdate(
+          id,
+          {
+            quantity: quntityDecrease[0],
+            totalPrice: quntityDecrease * price,
+          },
+          { new: true }
+        );
+        res.status(200).json({
+          data: cartUpdateAfterDecrement,
+          message: "quntity decrease",
+        });
+        break;
+      default:
+        console.log("something is wrong");
+        break;
+    }
+  } catch (e) {
+    console.log(e.message);
   }
 };
 
@@ -116,4 +165,5 @@ module.exports = {
   addToCart,
   fetchCartData,
   deleteCartItem,
+  updateCartItem,
 };
--- server/routes/cart.route.js
+++ server/routes/cart.route.js
@@ -3,11 +3,13 @@ const {
   addToCart,
   fetchCartData,
   deleteCartItem,
+  updateCartItem,
 } = require("../controller/cart.controller");
 const router = express.Router();
 
 router.post("/add", addToCart);
 router.get("/fetch", fetchCartData);
 router.delete("/delete/:id", deleteCartItem);
+router.put("/update", updateCartItem);
 
 module.exports = router;
