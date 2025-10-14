3e6a3e8fb119364a5f1658d0060f213b6a9db06b
Pawan <pawan@Pawans-MacBook-Air.local>
2025-10-14 12:29:19 +0530

    added api for add coupon

--- server/controller/coupon.controller.js
+++ server/controller/coupon.controller.js
@@ -1,6 +1,35 @@
 const Coupon = require("../models/coupon");
 
-const addCoupon = async (req, res) => {};
+const addCoupon = async (req, res) => {
+  try {
+    const c = req.body;
+    const createCouponObj = {
+      title: c.title,
+      code: c.code,
+      type: c.type,
+      value: c.value,
+      minimumOrder: c.minimumOrder,
+      useleft: c.useleft,
+      expireOn: c.expireOn,
+      active: c.active,
+    };
+
+    const createCoupon = await Coupon.create(createCouponObj);
+    if (createCoupon) {
+      return res.status(201).json({
+        success: true,
+        data: createCoupon,
+        message: "added coupon",
+      });
+    }
+  } catch (e) {
+    console.log(e.message);
+    return res.status(500).json({
+      message: "something is wrong",
+    });
+  }
+};
+
 const getAllCoupon = async (req, res) => {};
 const deleteCoupon = async (req, res) => {};
 const deleteAllCoupon = async (req, res) => {};
